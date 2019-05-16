c0c406707d047a02aaae3cc1215cccf69265c38a
donydex <donydex@163.com>
2019-05-16 19:32:11 +0800

    完成功能：
    1.完成底部导航栏
    2.index是获取用户头像
    3.log是显示登录日志

--- code/app.js
+++ code/app.js
@@ -1,20 +1,39 @@
-import util from './utils/index';
-let handler = {
-  //小程序初始化
-  onLaunch() {
-    console.log('app init...')
-    // 增加初始化缓存数据功能
-    util.getStorageData('visited', (data) => {
-      this.globalData.visitedArticles = data;
+//app.js
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
     })
   },
-  //小程序全局数据
   globalData: {
-    user: {
-      name: '',
-      avator: ''
-    },
-    visitedArticles: ''
+    userInfo: null
   }
-};
-App(handler);
+})
